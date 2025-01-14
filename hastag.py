662b1afb0fcc33c672b08b810ad8e7d8767a447e
Sazarr <sazar@ukr.net>
2025-01-14 20:22:14 +0200

    yep4

---
+++ hastag.py
@@ -0,0 +1,9 @@
+def generate_hashtag(s):
+    words = s.strip().split()
+    if not words:
+        return False
+    #first_s = s[0]
+    hashtag_text = "#" + ''.join(word.capitalize() for word in words)
+    if len(hashtag_text) > 140:
+        return False
+    return hashtag_text
