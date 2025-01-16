d0374e41ac81af465726f27a448a8bae9bc1b29b
Sazarr <sazar@ukr.net>
2025-01-16 16:17:44 +0200

    yep7

--- digits.py
+++ digits.py
@@ -7,3 +7,6 @@ def expanded_form(num):
     return " + ".join(parts)
 
 
+def expanded_form(num):
+    num = list(str(num))
+    return ' + '.join(x + '0' * (len(num) - y - 1) for y,x in enumerate(num) if x != '0')
