68c92c57e8da61b32e2aac93560f076b245afb1b
Sazarr <sazar@ukr.net>
2025-01-20 17:47:04 +0200

    woah

--- converting seconds.py
+++ converting seconds.py
@@ -4,3 +4,8 @@ def make_readable(seconds):
     seconds = seconds % 60
 
     return f"{hours:02}:{minutes:02}:{seconds:02}"
+
+def make_readable(seconds):
+    hours, seconds = divmod(seconds, 60 ** 2)
+    minutes, seconds = divmod(seconds, 60)
+    return '{:02}:{:02}:{:02}'.format(hours, minutes, seconds)
