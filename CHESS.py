0c995e1bc650fac7d29a4b5df19741af87ea876a
Sazarr <sazar@ukr.net>
2025-02-04 16:19:27 +0200

    hhmmm

--- CHESS.py
+++ CHESS.py
@@ -38,3 +38,30 @@ def knight(start, end):
                 visited.add((nx, ny))
 
     return -1  # This should never happen on a valid 8x8 board
+
+from collections import deque
+moves = ((1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1))
+
+def knight(p1, p2):
+    x, y = ord(p2[0])-97, int(p2[1])-1
+    left, seen = deque([(ord(p1[0])-97, int(p1[1])-1, 0)]), set()
+    while left:
+        i, j, v = left.popleft()
+        if i==x and j==y: return v
+        if (i, j) in seen: continue
+        seen.add((i, j))
+        for a,b in moves:
+            if 0 <= i+a < 8 and 0 <= j+b < 8:
+                left.append((i+a, j+b, v+1))
+
+
+def knight(p1, p2):
+    a, b = [('abcdefgh'.index(p[0]), int(p[1])) for p in [p1, p2]]
+    x, y = sorted((abs(a[0] - b[0]), abs(a[1] - b[1])))[::-1]
+
+    if (x, y) == (1, 0): return 3
+    if (x, y) == (2, 2) or ((x, y) == (1, 1) and any(p in ['a1', 'h1', 'a8', 'h8'] for p in [p1, p2])): return 4
+
+    delta = x - y
+
+    return delta - 2 * ((delta - y) // (3 if y > delta else 4))
