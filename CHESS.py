87dc293496cec823304f9e8dde647df02ab680ed
Sazarr <sazar@ukr.net>
2025-02-05 17:50:19 +0200

    hhmmm

--- CHESS.py
+++ CHESS.py
@@ -39,6 +39,19 @@ def knight(start, end):
 
     return -1  # This should never happen on a valid 8x8 board
 
+
+def knight(p1, p2):
+    a, b = [('abcdefgh'.index(p[0]), int(p[1])) for p in [p1, p2]]
+    x, y = sorted((abs(a[0] - b[0]), abs(a[1] - b[1])))[::-1]
+
+    if (x, y) == (1, 0): return 3
+    if (x, y) == (2, 2) or ((x, y) == (1, 1) and any(p in ['a1', 'h1', 'a8', 'h8'] for p in [p1, p2])): return 4
+
+    delta = x - y
+
+    return delta - 2 * ((delta - y) // (3 if y > delta else 4))
+
+
 from collections import deque
 moves = ((1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1))
 
@@ -55,13 +68,3 @@ def knight(p1, p2):
                 left.append((i+a, j+b, v+1))
 
 
-def knight(p1, p2):
-    a, b = [('abcdefgh'.index(p[0]), int(p[1])) for p in [p1, p2]]
-    x, y = sorted((abs(a[0] - b[0]), abs(a[1] - b[1])))[::-1]
-
-    if (x, y) == (1, 0): return 3
-    if (x, y) == (2, 2) or ((x, y) == (1, 1) and any(p in ['a1', 'h1', 'a8', 'h8'] for p in [p1, p2])): return 4
-
-    delta = x - y
-
-    return delta - 2 * ((delta - y) // (3 if y > delta else 4))
