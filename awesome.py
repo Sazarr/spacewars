cd1889651d5404e55d3ff5506409c03fb9741a4e
Sazarr <sazar@ukr.net>
2025-02-19 15:05:13 +0200

    norm

--- awesome.py
+++ awesome.py
@@ -29,3 +29,14 @@ def is_interesting(number, awesome_phrases):
     if check(number + 1) or check(number + 2):
         return 1
     return 0
+
+
+def is_good(n, awesome):
+    return n in awesome or str(n) in "1234567890 9876543210" or str(n) == str(n)[::-1] or int(str(n)[1:]) == 0
+
+def is_interesting(n, awesome):
+    if n > 99 and is_good(n, awesome):
+        return 2
+    if n > 97 and (is_good(n + 1, awesome) or is_good(n + 2, awesome)):
+        return 1
+    return 0
