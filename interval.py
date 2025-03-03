c134219ccd6c0698b8e8c3eb22ccf0d79da8692b
Sazarr <sazar@ukr.net>
2025-03-03 13:23:39 +0200

    hm

--- interval.py
+++ interval.py
@@ -47,3 +47,10 @@ def sum_intervals(intervals):
     return total_length
 
 
+def sum_of_intervals(intervals):
+    s, top = 0, float("-inf")
+    for a,b in sorted(intervals):
+        if top < a: top    = a
+        if top < b: s, top = s+b-top, b
+    return s
+
