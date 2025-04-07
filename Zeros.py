a86baf0f17ab51c1ec7ba4781c3f5f4fc163d4c7
Sazarr <sazar@ukr.net>
2025-04-07 17:35:15 +0300

    0

--- Zeros.py
+++ Zeros.py
@@ -5,8 +5,6 @@ def move_zeros(lst):
             lst[non_zero_index], lst[i] = lst[i], lst[non_zero_index]
             non_zero_index +=1
     return lst
-
-
 def move_zeros(array):
     for i in array:
         if i == 0:
