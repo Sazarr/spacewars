a7851ccb0b0a5444fc72d13034871a7dfa6562e9
Sazarr <sazar@ukr.net>
2025-04-09 18:40:19 +0300

    converting

--- domain.py
+++ domain.py
@@ -1,6 +1,5 @@
 import re
 def domain_name(url):
     return re.search(r"(?:https?://)?(?:www\.)?([^./]+)", url).group(1)
-
 def domain_name(url):
     return url.split("//")[-1].split("www.")[-1].split(".")[0]
